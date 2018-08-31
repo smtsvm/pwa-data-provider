eb60d7338e02876d8d122fd607300ad6be0127cb
SametSevim <samet307@gmail.com>
2018-08-31 14:38:04 +0300

    Initial

---
+++ src/main/java/smt/svm/pwadataprovider/PwaDataProviderApplication.java
@@ -0,0 +1,12 @@
+package smt.svm.pwadataprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwaDataProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwaDataProviderApplication.class, args);
+	}
+}
---
+++ src/test/java/smt/svm/pwadataprovider/PwaDataProviderApplicationTests.java
@@ -0,0 +1,16 @@
+package smt.svm.pwadataprovider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PwaDataProviderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
